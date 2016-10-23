148530a79e1bd7cbc9052a6da6ae86d9b07cbb63
TryExceptElse <SchwarzJohnV@gmail.com>
2016-10-22 21:36:58 -0700

    Added Association tests

---
+++ test/test_associations.py
@@ -0,0 +1,24 @@
+from leadmacro import Associations
+
+from tempfile import TemporaryFile
+
+from unittest import TestCase
+
+
+class TestAssociations(TestCase):
+    def setUp(self):
+        test_file_path = ''
+        self.assoc = Associations(test_file_path)
+        self.assoc.add_assoc('test_key_a', 'test_value_a1')
+        self.assoc.add_assoc('test_key_b', 'test_value_b1')
+        self.assoc.add_assoc('test_key_c', 'test_value_c1')
+        self.assoc.add_assoc('test_key_a', 'test_value_a2')
+
+    def test_get_item_returns_correct_values(self):
+        self.assertEqual('test_value_a1', self.assoc['test_key_a'][0])
+        self.assertEqual('test_value_b1', self.assoc['test_key_b'][0])
+        self.assertEqual('test_value_c1', self.assoc['test_key_c'][0])
+        self.assertEqual('test_value_a2', self.assoc['test_key_a'][1])
+
+    def test_assoc_can_be_cleaned(self):
+        limit
