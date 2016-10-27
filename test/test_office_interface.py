d43ead418646271394f0c78d0d103c88eba49f4a
tryexceptelse <->
2016-10-27 16:56:09 -0700

    added Tests

--- test/test_office_interface.py
+++ test/test_office_interface.py
@@ -11,7 +11,7 @@ import leadmacro
 import settings
 
 
-class TestOffice(TestCase):
+class TestOfficeObj(TestCase):
     test_resource_dir_name = 'test_resources'
     test_book_name = 'TestBook1.xlsx'
 
@@ -32,7 +32,6 @@ class TestOffice(TestCase):
                           'non-Windows system.')
         cls.app = xw.App(visible=False)  # create app
         assert isinstance(cls.app, xw.App)
-        cls.app.screen_updating = False
         # get test book
         path_to_book = os.path.join(
             settings.PROJECT_ROOT,
@@ -47,16 +46,17 @@ class TestOffice(TestCase):
         Shuts down app created for TestCellLine
         :return:
         """
-        cls.app.quit()
+        cls.app.kill()
 
     def setUp(self):
         self.model = leadmacro.Office.XW.Model(self.app)
         self.sheet = self.model['TestSheet1']
         assert isinstance(self.sheet, leadmacro.Sheet), self.sheet.__repr__()
 
+
+class TestCellLine(TestOfficeObj):
     def testCellLineIteratesOverTestRow1Correctly(self):
-        self.cell_line = leadmacro.CellLine(self.sheet, 'x', 1)
-        line_iterator = self.cell_line.__iter__()
+        line_iterator = leadmacro.CellLine(self.sheet, 'x', 1).__iter__()
         first_cell = line_iterator.__next__()
         self.assertIsInstance(first_cell, leadmacro.Cell)
         self.assertEquals("2a", first_cell.value)
@@ -67,8 +67,7 @@ class TestOffice(TestCase):
         self.assertEqual('string', string_cell.value)
 
     def testCellLineIteratorTerminatesAfterAllCellsReturned(self):
-        self.cell_line = leadmacro.CellLine(self.sheet, 'x', 1)
-        line_iterator = self.cell_line.__iter__()
+        line_iterator = leadmacro.CellLine(self.sheet, 'x', 1).__iter__()
         line_iterator.__next__()
         line_iterator.__next__()
         line_iterator.__next__()
@@ -78,3 +77,18 @@ class TestOffice(TestCase):
             self.assertRaises(StopIteration, line_iterator.__next__())
         except StopIteration:
             pass
+
+
+class TestLineSeries(TestOfficeObj):
+    def test_line_series_returns_each_column_in_turn(self):
+        reference_line = leadmacro.Office.XW.Row(self.sheet, 3, 0)
+        columns = [column for column in leadmacro.LineSeries(reference_line)]
+        self.assertEqual(2, len(columns))
+        self.assertIsInstance(columns[0], leadmacro.Column)
+        self.assertEqual('headerA', columns[0].name)
+        self.assertIsInstance(columns[1], leadmacro.Column)
+        self.assertEqual('headerB', columns[1].name)
+
+class TestRow(TestOfficeObj):
+    def test_row_iterator_returns_each_cell_in_row(self):
+        row = leadmacro.Office.XW.Row()
