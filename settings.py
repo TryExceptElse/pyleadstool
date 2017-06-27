48b59824c19352647b71de7b84542a57bb28bba5
TryExceptElse <SchwarzJohnV@gmail.com>
2017-06-27 11:44:40 -0700

    organized settings.py file, added preferences path.

--- settings.py
+++ settings.py
@@ -1,10 +1,14 @@
 import os
 import appdirs
 
+# project file/dir paths
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 UI_INPUT_DIR = os.path.join(PROJECT_ROOT, 'ui')
 UI_OUTPUT_DIR = os.path.join(PROJECT_ROOT, 'leads', 'ui', 'layout')
+RESOURCE_PATH = os.path.join(PROJECT_ROOT, 'resources')
+
+# app data file/dir paths
 APP_DATA_DIR = appdirs.user_data_dir('leads_tool')
 DB_FILE_PATH = os.path.join(APP_DATA_DIR, 'db')
 CAMPAIGNS_PATH = os.path.join(APP_DATA_DIR, 'campaigns')
-RESOURCE_PATH = os.path.join(PROJECT_ROOT, 'resources')
+PREF_PATH = os.path.join(APP_DATA_DIR, 'pref')
